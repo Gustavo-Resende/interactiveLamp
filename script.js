3888a5b970f7a7010b60ea759e8cb4049582ea32
Danilo Trindade <danilotrindade87@gmail.com>
2022-11-06 14:01:42 -0300

    troca lampada quando estiver quebrada

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const ligar = document.getElementById('on');
 const desligar = document.getElementById('off');
+const trocarLampada = document.getElementById('newLamp');
 const lamp = document.getElementById('lamp')
 
 function isLampBroken() {
@@ -20,6 +21,17 @@ function lampOff() {
 
 function lampBrake() {
     lamp.src = 'assets/brake.jpg'
+    showButton();
+}
+
+// troca lâmpada quando é chamado pela função changeLamp
+function lampNew() {
+    lamp.src = 'assets/off.jpg';
+}
+
+// mostra botão para trocar lampada, quando estiver quebrada
+function showButton() {
+    trocarLampada.style.display = 'block';
 }
 
 ligar.addEventListener ('click', lampOn)
@@ -34,5 +46,18 @@ ligar.onclick = function() {
     clicksOn++;
     if(clicksOn > 3){
         lamp.src = 'assets/brake.jpg'
+        showButton();
     }
+}
+
+/**
+ * função chamada pela ação onclick do botão TROCAR
+ * (1) zera variável clicksOn
+ * (2) troca lâmpada
+ * (3) esconde botão usado para trocar lâmpada
+ */
+function changeLamp() {
+    clicksOn = 0;
+    lampNew();
+    trocarLampada.style.display = 'none';
 }
